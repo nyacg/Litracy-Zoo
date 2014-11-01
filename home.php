6ea0fede8d1b8eca884e4e855670679e96045d80
nyacg <robert.j.h.chandler@googlemail.com>
2014-11-01 14:29:02 +0000

    Very basic first stuff

---
+++ home.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Litracy Zoo</title>
+	</head>
+
+	<body>
+		<div id='canvas'>
+
+		</div>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	</body>
+</html>
