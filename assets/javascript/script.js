12f96fe8502e48e3ec173f9233e252f8ce9488e5
nyacg <robert.j.h.chandler@googlemail.com>
2014-11-01 16:15:24 +0000

    4 o'clock

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,27 @@
-document.ready(function(){
+$(document).ready(function(){
 
+	//setSize();
+	$('#name-input').focus();
 
+	$('#go-button').click(function(){
+		nameToPet();
+	});
+
+	$(document).keypress(function(e) {
+		if(e.which == 13){
+			nameToPet();
+		}
+	});
 
 });
+
+function setSize(){
+	var $canvas = $('#canvas');
+	$cavas.width(window.width());
+	$canvas.height(window.height());
+}
+
+function nameToPet(){
+	$('#name-page').hide();
+	$('#pet-page').show();
+}
--- home.php
+++ home.php
@@ -1,12 +1,31 @@
 <html>
 	<head>
+		<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
 		<title>Litracy Zoo</title>
+		<link rel='stylesheet' type='text/css' href='./assets/stylesheets/style.css' />
+		<link href="//fonts.googleapis.com/css?family=Signika:300,400,600,700" rel="stylesheet" type="text/css">
 	</head>
 
 	<body>
 		<div id='canvas'>
+			<div id='name-page'>
+				<h1>Hello, welcome to Literacy Zoo</h1>
+				<h2>What is your name?</h2>
+				<input name='name' id='name-input' type='text' autocomplete='off'></input>
+				<img src='./assets/images/go.png' id='go-button'/>
+			</div>
 
+			<div id='pet-page' class='hidden'>
+				<h2>Select and name your first animal</h2>
+				<img src='./assets/images/c_icon.png'>
+				<img src='./assets/images/c_icon.png'>
+				<img src='./assets/images/c_icon.png'>
+				<img src='./assets/images/c_icon.png'>
+			</div>
 		</div>
+
+
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<script type='text/javascript' src='./assets/javascript/script.js'></script>
 	</body>
 </html>
